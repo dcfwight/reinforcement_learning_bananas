3fa879500ee29e22d014286c0c80ead825bf2e90
Doug Wight <dcfwight@gmail.com>
2021-01-24 17:01:35 +0000

    Import Numpy

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ import random
 import torch.optim as optim
 from collections import namedtuple, deque
 import torch
+import numpy as np
 
 # set up the hyperparameters to use
 BUFFER_SIZE = int(1e5) # replay experience buffer size. Original 1e5
